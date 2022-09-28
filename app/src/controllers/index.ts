9787a08e87afb671af5061fa04eb218398ace001
sankaire <kerenketepela@gmail.com>
2022-09-28 20:55:24 +0300

    chore:🚀 export all functions of each dir in one entry file

--- app/src/controllers/index.ts
+++ app/src/controllers/index.ts
@@ -1,4 +1,7 @@
 import {Request, Response} from "express";
+import {signUp} from "./auth/auth";
+
+export default signUp
 
 export const home =  (req:Request, res:Response) => {
     return res.status(200).json({message: 'Welcome to Wicode-cms api v1'});

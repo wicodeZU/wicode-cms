d42dcab760656827555b65ab4aacc84d782e5ac7
sankaire <kerenketepela@gmail.com>
2022-09-28 20:56:06 +0300

    feat:💡 register auth route

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import {homeRouter} from "./api/routes/";
 import {dbConnection} from "./config/dbConnect";
+import {authRoute, homeRouter} from "./api/routes";
+
 
 export const app = express();
 
@@ -9,6 +10,7 @@ dbConnection()
 app.use(express.json());
 
 app.use("/api/v1", homeRouter)
+app.use("/api/v1/auth", authRoute)
 
 app.listen(5000, () => {
     console.log(`Server is running on port http://localhost:5000`);
